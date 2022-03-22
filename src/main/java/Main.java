a2ccd397d4fd81c90d71f0b5c8a2ac0b02b87889
Vusani <79424556+Vusani-Makhomu@users.noreply.github.com>
2022-03-23 00:51:24 +0200

    Able to retrieve data from API and store it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Fetching data from API...");
+        try(java.io.InputStream is = new java.net.URL("http://sam-user-activity.eu-west-1.elasticbeanstalk.com/").openStream()) {
+            String contents = new String(is.readAllBytes());
+            JsonElement element = JsonParser.parseString(contents);
+            Object[] apiData = element.getAsJsonObject().entrySet().toArray();
+            System.out.println("Printing out datat...");
+
+            for (int index=0; index<apiData.length;index++) {
+                System.out.println(apiData[index]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
