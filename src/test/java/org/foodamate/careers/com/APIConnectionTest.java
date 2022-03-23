dc50f25e732b3db798546090f7835b18e0a98689
Vusani <79424556+Vusani-Makhomu@users.noreply.github.com>
2022-03-24 00:32:01 +0200

    Renamed NetworkTest to APIConnectionTest

--- src/test/java/org/foodamate/careers/com/APIConnectionTest.java
+++ src/test/java/org/foodamate/careers/com/APIConnectionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class NetworkTest {
+public class APIConnectionTest {
 
     private final static APIConnection apiConnection = new APIConnection("http://sam-user-activity.eu-west-1.elasticbeanstalk.com/");
 
