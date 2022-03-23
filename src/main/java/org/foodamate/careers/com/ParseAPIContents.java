d8e296f373308c4f76cc62fbeb98fc5777586efa
Vusani <79424556+Vusani-Makhomu@users.noreply.github.com>
2022-03-23 22:22:41 +0200

    Added ParseAPIContents for parsing data fetched from API and returning it in array.

---
+++ src/main/java/org/foodamate/careers/com/ParseAPIContents.java
@@ -0,0 +1,35 @@
+package org.foodamate.careers.com;
+
+import java.util.Arrays;
+
+public class ParseAPIContents {
+    private String apiData;
+    private String[] parsedApiData;
+
+    public ParseAPIContents(String contents) {
+        apiData = contents;
+    }
+
+    private void removeCurlyBraces() {
+        apiData = apiData.replace("{", "");
+        apiData = apiData.replace("}", "");
+        apiData = apiData.strip();
+    }
+
+    private String[] returnDataInArray() {
+        return apiData.split(",");
+    }
+
+    public void parseApiData() {
+        removeCurlyBraces();
+        parsedApiData = returnDataInArray();
+        for (int index=0;index<parsedApiData.length;index++) {
+            parsedApiData[index] = parsedApiData[index].replace("\"", "").strip();
+            parsedApiData[index] = parsedApiData[index].replace(":", "=");
+        }
+    }
+
+    public String[] getParsedApiData() {
+        return Arrays.copyOf(parsedApiData, parsedApiData.length);
+    }
+}
