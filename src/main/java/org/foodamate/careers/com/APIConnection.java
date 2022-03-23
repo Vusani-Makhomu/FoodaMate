5b56f9a8c0ea4d7e0eb5c10cf1d6f5ee5bcfe9f1
Vusani <79424556+Vusani-Makhomu@users.noreply.github.com>
2022-03-23 23:39:43 +0200

    Added APIConnection.java

---
+++ src/main/java/org/foodamate/careers/com/APIConnection.java
@@ -0,0 +1,38 @@
+package org.foodamate.careers.com;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class APIConnection {
+    private boolean connected;
+    private String apiContents;
+    private String url;
+
+    public APIConnection(String url) {
+        this.url = url;
+    }
+
+    public void retrieveAPIContents() {
+        try (InputStream inputStream = new URL(url).openStream()) {
+            apiContents = new String(inputStream.readAllBytes());
+            connected = true;
+        } catch (IOException e) {
+            connected = false;
+        }
+    }
+
+    public boolean isConnected() {
+        return  connected;
+    }
+
+    public String getApiContents() {
+        return apiContents;
+    }
+
+    public String errorMessage() {
+        return "***An error occurred. Program failed to fetch data from API***\n"+
+                "***Please try again.***\n"+
+                "***If error persists, please check if the API is running:"+url+"***";
+    }
+}
