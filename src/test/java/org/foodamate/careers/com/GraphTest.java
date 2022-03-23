62046fccd459e6b29339de3ab83308dd72580c80
Vusani <79424556+Vusani-Makhomu@users.noreply.github.com>
2022-03-24 00:27:46 +0200

    Added a before all annotation to retrieve api data and set it as graph data. Added test to check if correct number of asterisks are returned and to check if the correct graph total value is returned.

--- src/test/java/org/foodamate/careers/com/GraphTest.java
+++ src/test/java/org/foodamate/careers/com/GraphTest.java
@@ -1,4 +1,43 @@
 package org.foodamate.careers.com;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class GraphTest {
+    private final static APIConnection apiConnection = new APIConnection("http://sam-user-activity.eu-west-1.elasticbeanstalk.com/");
+    static ParseAPIContents parseAPIContents = new ParseAPIContents();
+    private final static Graph graph = new Graph();
+
+    @BeforeAll
+    static void setGraphData() {
+        apiConnection.retrieveAPIContents();
+        parseAPIContents.setApiData(apiConnection.getApiContents());
+        parseAPIContents.parseApiData();
+        graph.setGraphData(parseAPIContents.getParsedApiData());
+    }
+
+    @Test
+    void correctNumAsterisks() {
+        assertEquals("**********", graph.returnNumAsterisks(10));
+    }
+
+    @Test
+    void incorrectNumAsterisks() {
+        assertNotEquals("***", graph.returnNumAsterisks(10));
+    }
+
+    @Test
+    void correctGraphTotal() {
+        graph.setStartDate("01-01-2022");
+        graph.setEndDate("15-01-2022");
+        assertEquals(285800.0, graph.calculateGraphTotalValue());
+    }
+
+//    @Test
+//    void incorrectGraphTotal() {
+//
+//    }
+
 }
